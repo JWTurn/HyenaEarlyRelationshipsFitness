fe624c045d132d29df92b1126e618f7b14ebccdd
Alec Robitaille <robit.alec@gmail.com>
2019-03-01 16:19:08 -0800

    build association gbi and net

--- R/1-HyenaSpatsoc.R
+++ R/1-HyenaSpatsoc.R
@@ -3,9 +3,8 @@
 # March 01 2019
 
 ### Packages ----
-libs <- c('data.table', 'spatsoc')
+libs <- c('data.table', 'spatsoc', 'asnipe')
 lapply(libs, require, character.only = TRUE)
-
 
 ### Import data ----
 raw <- dir('data/raw-data', full.names = TRUE)
@@ -14,7 +13,6 @@ affil <- fread(raw[grepl('affil', raw)])
 aggr <- fread(raw[grepl('agg', raw)])
 asso <- fread(raw[grepl('asso', raw)])
 egos <- fread(raw[grepl('egos', raw)])
-
 
 ### Prep ----
 affil[, sessiondate := as.IDate(sessiondate)]
@@ -28,5 +26,15 @@ asso[, sessiondate := as.IDate(sessiondate)]
 egos[, period_start := as.IDate(period_start)]
 egos[, period_end := as.IDate(period_end)]
 
+### Association ----
+# Cast session to an integer group column
+asso[, group := .GRP, session]
 
-###
+# Build group by individual matrix
+gbiMtrx <- get_gbi(asso, group = 'group', id = 'hyena')
+
+## Generate observed network
+#TODO: what association index?
+net <- get_network(gbiMtrx,
+									 data_format = "GBI",
+									 association_index = "SRI")
