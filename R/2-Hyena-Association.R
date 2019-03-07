94bfb7f847c06fe0e24afee2508d677f2f8fe10e
Alec Robitaille <robit.alec@gmail.com>
2019-03-07 11:32:20 -0800

    fin setup associations for ego centric

--- R/2-Hyena-Association.R
+++ R/2-Hyena-Association.R
@@ -9,6 +9,8 @@
 libs <- c('data.table', 'spatsoc', 'asnipe', 'igraph', 'foreach')
 lapply(libs, require, character.only = TRUE)
 
+
+
 ### Import data ----
 derived <- dir('data/derived-data', full.names = TRUE)
 raw <- dir('data/raw-data', full.names = TRUE)
@@ -18,6 +20,8 @@ asso <- readRDS(derived[grepl('association-life', derived)])
 ## Life stages
 life <- fread(raw[grepl('lifeperiods.csv', raw)], drop = 'V1')
 
+
+
 ### Prep ----
 # Date columns
 asso[, sessiondate := as.IDate(sessiondate)]
@@ -25,7 +29,6 @@ asso[, yr := year(sessiondate)]
 
 periods <- c('period_start', 'period_end')
 life[, (periods) := lapply(.SD, as.IDate), .SDcols = (periods)]
-
 
 # Cast session to an integer group column
 asso[, group := .GRP, session]
@@ -36,60 +39,47 @@ life <- life[, .(ego, period, period_start, period_end)]
 groupCol <- 'group'
 idCol <- 'hyena'
 
-### For each ego*life stage ----
-# life <- life[1:5]
+
+
+### Make networks for each ego*life stage ----
+# Set up parallel with doParallel and foreach
+doParallel::registerDoParallel()
+
+life <- life[1:5]
 
 # To avoid the merge dropping out sessiondate to sessiondate and sessiondate.i (matching period start and end), we'll add it as an extra column and disregard those later
 asso[, idate := sessiondate]
 
-# Loop through each ego's life stages
-system.time(gbiLs <- lapply(seq(1, nrow(life)), function(i) {
+# Generate a GBI for each ego's life stage
+gbiLs <- foreach(i = seq(1, nrow(life))) %dopar% {
 	sub <- asso[life[i],
 							on = .(sessiondate >= period_start,
 										 sessiondate < period_end)]
-	sub[, id := ifelse(hyena == life[i]$ego,
-										 idlife, hyena)]
-	get_gbi(sub, group = groupCol, id = 'id')
-}))
+	sub[, id := ifelse(hyena == life[i]$ego, idlife, hyena)]
+	get_gbi(sub, 'group', 'id')
+}
 
-doParallel::registerDoParallel()
-system.time(gbiLs <- foreach(i = seq(1, nrow(life))) %dopar% {
-	sub <- asso[life[i],
-							on = .(sessiondate >= period_start,
-										 sessiondate < period_end)]
-	sub[, id := ifelse(hyena == life[i]$ego,
-										 idlife, hyena)]
-	get_gbi(sub, group = groupCol, id = 'id')
-})
+# Generate list of networks
+netLs <- foreach(g = gbiLs) %dopar% {
+	get_network(g, data_format = "GBI", association_index = "HWI")
+}
 
-netLs <- lapply(gbiLs, FUN = get_network,
-								data_format = "GBI",
-								association_index = "HWI")
-
-## Generate graph and calculate network metrics
-mets <- lapply(seq_along(netLs), function(n) {
+# Generate graph and calculate network metrics
+mets <- foreach(n = seq_along(netLs)) %dopar% {
 	g <- graph.adjacency(netLs[[n]], 'undirected',
 											 diag = FALSE, weighted = TRUE)
 
-	# TODO: Are these the network metrics you want? Add them here...
-	# degree, outdegree, indegree, strength, outstrength, instrength, betweenness
 	cbind(data.table(
 		degree = degree(g),
 		strength = strength(g),
 		betweenness = betweenness(g, directed = FALSE),
 		ID = names(degree(g))
 	), life[n])
-})
+}
 
-## Observed and random for all individuals across all iterations and years
 out <- rbindlist(mets)
 setnames(out, 'ID', idCol)
 
-## Split observed and random
-out[, observed := ifelse(iteration == 0, TRUE, FALSE)]
 
-## Mean values for each individual and year, by observed/random
-meanMets <- out[, lapply(.SD, mean), by = c(idCol, 'yr', 'observed'),
-								.SDcols = colnames(out)[1:7]]
-
-saveRDS(meanMets, 'data/derived-data/mean-mets-association.Rds')
+### Output ----
+out
