4217620ca17c5934142bb84f48099d54a15ee0c5
Alec Robitaille <robit.alec@gmail.com>
2019-03-15 18:03:11 -0700

    add get_gbi_t for temporal gbi

--- R/hwi.R
+++ R/hwi.R
@@ -7,12 +7,38 @@ library(hwig)
 library(spatsoc)
 library(data.table)
 data(DT)
-gbi <- get_gbi(DT[id <= 5], 'group', 'id')
 
-## If we assume that all groups are temporally distinct
-# 	then yab is 0
+edges <- readRDS('data/derived-data/single-focal-life-stage.Rds')
+edges
+
+
+get_gbi_t <- function(DT = NULL, group = "group", id = NULL, timegroup = NULL){
+	uDT <- stats::na.omit(unique(DT[, .SD, .SDcols = c(group,
+																										 id)]), cols = group)
+	cDT <-
+		data.table::dcast(
+			uDT,
+			formula = stats::reformulate(id, c(group, timegroup)),
+			fun.aggregate = length,
+			value.var = group
+		)
+	ids <- colnames(cDT)[!grepl(group, colnames(cDT))]
+	m <- as.matrix(cDT[, .SD, .SDcols = ids])
+	rownames(m) <- cDT[[group]]
+	return(m)
+}
+
+gbi <- get_gbi(edges, 'group', 'hyena')
+gbit <- get_gbi_t(edges, 'group', 'hyena', 'session')
+
 gbi <- gbi[1:5,]
+
+
+# spread a 1 in a timegroup, to all rows of that timegroup
+
 hwi <- function(gbi, times) {
+	## If we assume that all groups are temporally distinct
+	# 	then yab is 0
 	if (missing(times)) {
 		do.call(rbind,
 						lapply(seq_len(ncol(gbi)), function(i) {
@@ -29,47 +55,3 @@ hwi <- function(gbi, times) {
 						}))
 	}
 }
-if (missing(times)) {
-	do.call(rbind,
-					lapply(seq_len(ncol(gbi)), function(i) {
-						m <- matrix(rep(gbi[, i], ncol(gbi)), ncol = ncol(gbi))
-						x <- apply(m + gbi, 2, function(x)
-							sum(x == 2))
-						ya <- apply(m - gbi, 2, function(x)
-							sum(x == 1))
-						yb <- apply(m - gbi, 2, function(x)
-							sum(x == -1))
-						yab <- ya + yb
-
-						# x/(x + 0.5 * yab)
-						x / ((0.5 * (ya + yb)) + yab + x)
-					}))
-}
-
-
-
-
-# hwi <- function(gbi, time)
-do.call(rbind,
-lapply(seq_len(ncol(gbi)), function(i) {
-	m <- matrix(rep(gbi[, i], ncol(gbi)), ncol = ncol(gbi))
-	x <- apply(m + gbi, 2, function(x) sum(x == 2))
-	ya <- apply(m - gbi, 2, function(x) sum(x == 1))
-	yb <- apply(m - gbi, 2, function(x) sum(x == -1))
-	yab <- ya + yb
-
-	# x/(x + 0.5 * yab)
-	x / ((0.5 * (ya + yb)) + yab + x)
-})
-)
-
-
-
-##
-
-asso[, t := .GRP, .(sessiondate, session)]
-asso[, group := .GRP, session]
-DT <- asso[year(sessiondate) == 1988][hyena %chin% DT[, unique(hyena)[1:5]]]
-DT
-gbi <- get_gbi(DT, 'group', 'hyena')
-asnipe::get_network(gbi, 'GBI', 'HWI', times = seq(1:69))
