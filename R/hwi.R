7f40ae8b65f677a3cfd36256ff7049966a915c9f
Alec Robitaille <robit.alec@gmail.com>
2019-03-15 17:21:52 -0700

    solve yab

--- R/hwi.R
+++ R/hwi.R
@@ -3,14 +3,53 @@
 # Yb: swap a for b above
 # Yab: number of times both individuals were observed but in different groups
 
-
-
 library(hwig)
 library(spatsoc)
 library(data.table)
 data(DT)
 gbi <- get_gbi(DT[id <= 5], 'group', 'id')
 
+## If we assume that all groups are temporally distinct
+# 	then yab is 0
+gbi <- gbi[1:5,]
+hwi <- function(gbi, times) {
+	if (missing(times)) {
+		do.call(rbind,
+						lapply(seq_len(ncol(gbi)), function(i) {
+							m <- matrix(rep(gbi[, i], ncol(gbi)), ncol = ncol(gbi))
+							x <- apply(m + gbi, 2, function(x)
+								sum(x == 2))
+							ya <- apply(m - gbi, 2, function(x)
+								sum(x == 1))
+							yb <- apply(m - gbi, 2, function(x)
+								sum(x == -1))
+							yab <- 0
+
+							x / ((0.5 * (ya + yb)) + yab + x)
+						}))
+	}
+}
+if (missing(times)) {
+	do.call(rbind,
+					lapply(seq_len(ncol(gbi)), function(i) {
+						m <- matrix(rep(gbi[, i], ncol(gbi)), ncol = ncol(gbi))
+						x <- apply(m + gbi, 2, function(x)
+							sum(x == 2))
+						ya <- apply(m - gbi, 2, function(x)
+							sum(x == 1))
+						yb <- apply(m - gbi, 2, function(x)
+							sum(x == -1))
+						yab <- ya + yb
+
+						# x/(x + 0.5 * yab)
+						x / ((0.5 * (ya + yb)) + yab + x)
+					}))
+}
+
+
+
+
+# hwi <- function(gbi, time)
 do.call(rbind,
 lapply(seq_len(ncol(gbi)), function(i) {
 	m <- matrix(rep(gbi[, i], ncol(gbi)), ncol = ncol(gbi))
@@ -20,7 +59,7 @@ lapply(seq_len(ncol(gbi)), function(i) {
 	yab <- ya + yb
 
 	# x/(x + 0.5 * yab)
-	x/((0.5*(ya+yb)) + yab + x)
+	x / ((0.5 * (ya + yb)) + yab + x)
 })
 )
 
