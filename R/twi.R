9f181101095e13e7aa1ef9899816e682f3fddc92
Alec Robitaille <robit.alec@gmail.com>
2019-03-18 11:36:44 -0700

    add twice-weight code

---
+++ R/twi.R
@@ -0,0 +1,22 @@
+twi <- function(gbi, times) {
+	## If we assume that all sessions are temporally distinct
+	# 	then yab is 0
+	#   ya is all times where a is observed and not b
+	#   yb is all times where b is observed and not a
+	#   x is all times where a and b are observed associating
+	if (missing(times)) {
+		do.call(rbind,
+						lapply(seq_len(ncol(gbi)), function(i) {
+							m <- matrix(rep(gbi[, i], ncol(gbi)), ncol = ncol(gbi))
+							x <- apply(m + gbi, 2, function(x)
+								sum(x == 2))
+							ya <- apply(m - gbi, 2, function(x)
+								sum(x == 1))
+							yb <- apply(m - gbi, 2, function(x)
+								sum(x == -1))
+							yab <- 0
+
+							x / (x + (2 * yab) + ya + yb)
+						}))
+	}
+}
