011fdd376ee8fbfef4a5650e7b0b4e811c14b37b
Alec Robitaille <robit.alec@gmail.com>
2019-03-28 10:53:15 -0700

    add weights to strength aggression

--- R/4-Hyena-Aggression.R
+++ R/4-Hyena-Aggression.R
@@ -79,9 +79,9 @@ mets <- foreach(i = seq_along(edgeLs)) %dopar% {
 			aggr_degree = degree(g, mode = 'total'),
 			aggr_outdegree = degree(g, mode = 'out'),
 			aggr_indegree = degree(g, mode = 'in'),
-			aggr_strength = strength(g, mode = 'total'),
-			aggr_outstrength = strength(g, mode = 'out'),
-			aggr_instrength = strength(g, mode = 'in'),
+			aggr_strength = strength(g, mode = 'total', weights = (1/w)),
+			aggr_outstrength = strength(g, mode = 'out', weights = (1/w)),
+			aggr_instrength = strength(g, mode = 'in', weights = (1/w)),
 			aggr_betweenness = betweenness(g, directed = TRUE,
 																		 weights = (1/w)),
 			ID = names(degree(g))
