0e96ee152a33e18637659adf83f27b1d51d31021
Alec Robitaille <robit.alec@gmail.com>
2019-03-28 13:24:18 -0700

    set up a model by iteration

--- R/6-Models.R
+++ R/6-Models.R
@@ -35,15 +35,19 @@ DT <- merge(mets, egos, all.x = TRUE)
 
 ### Models ----
 DT[, {
-	m <- glmer(log(longevity_years) ~ ais_str_scl
-				+ agg_str_scl
-				+ ll_str_scl
-				+ ego_period_rank + offset(log(clan_size)) + offset(log(sessions_count)) + (1|mom),
-				data=.SD, family = 'gaussian')
+	m <- glmer(
+		log(longevity_years) ~ twi_strength +
+			aggr_strength +
+			affil_strength +
+			ego_period_rank +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
 	tidy(m)
 }, by = iteration]
-
-From the models, I would need to estimate and z score for each parameter
 
 
 ## best models for longevity CD
