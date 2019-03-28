423d146a992e93f3337136965918ce8358d5ec52
Alec Robitaille <robit.alec@gmail.com>
2019-03-27 20:52:51 -0700

    count sessions associations

--- R/2-Hyena-Association.R
+++ R/2-Hyena-Association.R
@@ -20,10 +20,23 @@ life <- readRDS(derived[grepl('ego-life', derived)])
 groupCol <- 'group'
 idCol <- 'hyena'
 
-### Make networks for each ego*life stage ----
 # Set up parallel with doParallel and foreach
 doParallel::registerDoParallel()
 
+### Count number of sessions and alone session ----
+# Generate a GBI for each ego's life stage
+nsesh <- rbindlist(foreach(i = seq(1, nrow(life))) %dopar% {
+	sub <- asso[life[i],
+							on = .(sessiondate >= period_start,
+										 sessiondate < period_end)]
+	ego <- sub$ego[[i]]
+	uasso <- unique(sub[, .(hyena, session)])
+	uasso[, nSession := .N, session]
+	nsesh <- uasso[, .(nSession = .N, nAlone = sum(nSession == 1)), hyena]
+	return(nsesh[hyena == ego])
+})
+
+### Make networks for each ego*life stage ----
 life <- life[1:5]
 
 # To avoid the merge dropping out sessiondate to sessiondate and sessiondate.i (matching period start and end), we'll add it as an extra column and disregard those later
@@ -64,6 +77,8 @@ out <- rbindlist(mets)
 setnames(out, 'ID', idCol)
 
 out <- out[hyena == ego]
+
+out <- merge(out, nsesh)
 
 ### Output ----
 saveRDS(out, 'data/derived-data/association-metrics.Rds')
