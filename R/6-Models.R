94cd8f8ad2c2623cb95ae3aead0a9e64307a74ac
Julie Turner <julwturner@gmail.com>
2019-04-02 17:54:24 -0230

    updated models

--- R/6-Models.R
+++ R/6-Models.R
@@ -35,13 +35,36 @@ DT <- merge(mets, egos, all.x = TRUE)
 
 ### Models ----
 # best models for longevity CD
-DT[period == 'cd', {
+DT.lcd.7 <- DT[period == 'cd', {
 	m <- glmer(
 		log(longevity_years) ~
+			ego_period_rank +
+			scale(twi_degree) +
+			scale(aggr_outdegree) + scale(aggr_indegree) +
+			scale(affil_outstrength) + scale(affil_instrength) +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
+	tidy(m)
+}, by = iteration]
+
+l.cd.7 <- glmer(log(longevity_years) ~  ais_deg_scl
+								+ agg_outdeg_scl + agg_indeg_scl
+								+ ll_outdeg_scl + ll_indeg_scl
+								+ ego_period_rank + offset(log(clan_size)) + offset(log(nSession)) + (1|mom),
+								data=data.f.long.cd, family = 'gaussian')
+
+
+DT.lcd.19 <- DT[period == 'cd', {
+	m <- glmer(
+		log(longevity_years) ~
+			ego_period_rank +
 			scale(twi_strength) +
 			scale(aggr_strength) +
 			scale(affil_strength) +
-			ego_period_rank +
 			offset(log(clan_size)) +
 			offset(log(nSession)) +
 			(1 | mom),
@@ -58,16 +81,52 @@ l.cd.19 <- glmer(log(longevity_years) ~ ais_str_scl
 								 + ego_period_rank + offset(log(clan_size)) + offset(log(sessions_count)) + (1|mom),
 								 data=data.f.long.cd, family = 'gaussian')
 
-# dAIC 0.6
-l.cd.7 <- glmer(log(longevity_years) ~  ais_deg_scl
-								+ agg_outdeg_scl + agg_indeg_scl
-								+ ll_outdeg_scl + ll_indeg_scl
-								+ ego_period_rank + offset(log(clan_size)) + offset(log(sessions_count)) + (1|mom),
-								data=data.f.long.cd, family = 'gaussian')
+
+DT.lcd.2 <- DT[period == 'cd', {
+	m <- glmer(
+		log(longevity_years) ~
+			ego_period_rank +
+			scale(alone) +
+			scale(twi_degree) +
+			scale(aggr_outdegree) + scale(aggr_indegree) +
+			scale(affil_outdegree) + scale(affil_indegree) +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
+	tidy(m)
+}, by = iteration]
+
+
+
+l.cd.2.a <- glmer(log(longevity_years) ~  alone_scl + ais_deg_scl
+									+ agg_outdeg_scl + agg_indeg_scl
+									+ ll_outdeg_scl + ll_indeg_scl
+									+ ego_period_rank + offset(log(clan_size)) + offset(log(nSession)) + (1|mom),
+									data=data.f.long.cd, family = 'gaussian')
+
 
 
 
 ## best model for longevity DI
+DT.lpg.18 <- DT[period == 'pg', {
+	m <- glmer(
+		log(longevity_years) ~
+			ego_period_rank +
+			scale(twi_degree) +
+			scale(aggr_degree) +
+			scale(affil_degree) +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
+	tidy(m)
+}, by = iteration]
+
 l.pg.18 <- glmer(log(longevity_years) ~ ais_deg_scl
 								 + agg_deg_scl
 								 + ll_deg_scl
@@ -77,6 +136,23 @@ l.pg.18 <- glmer(log(longevity_years) ~ ais_deg_scl
 
 
 ## best model for longevity Adult
+DT.lad.18 <- DT[period == 'adult', {
+	m <- glmer(
+		log(longevity_years) ~
+			ego_period_rank +
+			scale(twi_degree) +
+			scale(aggr_degree) +
+			scale(affil_degree) +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
+	tidy(m)
+}, by = iteration]
+
+
 l.ad.18 <- glmer(log(longevity_years) ~ ais_deg_scl
 								 + agg_deg_scl
 								 + ll_deg_scl
@@ -87,6 +163,22 @@ l.ad.18 <- glmer(log(longevity_years) ~ ais_deg_scl
 
 
 ## best models for ARS CD
+DT.acd.18 <- DT[period == 'cd', {
+	m <- glmer(
+		log(annual_rs) ~
+			ego_period_rank +
+			scale(twi_degree) +
+			scale(aggr_degree) +
+			scale(affil_degree) +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
+	tidy(m)
+}, by = iteration]
+
 a.cd.18 <- glmer(log(annual_rs) ~ ais_deg_scl
 								 + agg_deg_scl
 								 + ll_deg_scl
@@ -94,6 +186,23 @@ a.cd.18 <- glmer(log(annual_rs) ~ ais_deg_scl
 								 data=data.f.ars.cd, family = 'gaussian')
 
 # dAIC = 0.5
+DT.acd.11 <- DT[period == 'cd', {
+	m <- glmer(
+		log(annual_rs) ~
+			ego_period_rank +
+			scale(alone) +
+			scale(twi_degree) +
+			scale(aggr_degree) +
+			scale(affil_degree) +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
+	tidy(m)
+}, by = iteration]
+
 a.cd.11 <- glmer(log(annual_rs) ~ alone_scl + ais_deg_scl
 								 + agg_deg_scl
 								 + ll_deg_scl
@@ -103,6 +212,40 @@ a.cd.11 <- glmer(log(annual_rs) ~ alone_scl + ais_deg_scl
 
 
 ## best models for ARS DI
+DT.apg.18 <- DT[period == 'pg', {
+	m <- glmer(
+		log(annual_rs) ~
+			ego_period_rank +
+			scale(twi_degree) +
+			scale(aggr_degree) +
+			scale(affil_degree) +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
+	tidy(m)
+}, by = iteration]
+
+
+
+DT.apg.19 <- DT[period == 'pg', {
+	m <- glmer(
+		log(annual_rs) ~
+			ego_period_rank +
+			scale(twi_strength) +
+			scale(aggr_strength) +
+			scale(affil_strength) +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
+	tidy(m)
+}, by = iteration]
+
 a.pg.19 <- glmer(log(annual_rs) ~ ais_str_scl
 								 + agg_str_scl
 								 + ll_str_scl
@@ -119,11 +262,22 @@ a.pg.18 <- glmer(log(annual_rs) ~ ais_deg_scl
 
 
 ## best models for ARS Adult
-a.ad.19 <- glmer(log(annual_rs) ~ ais_str_scl
-								 + agg_str_scl
-								 + ll_str_scl
-								 + ego_period_rank + offset(log(clan_size)) + offset(log(sessions_count)) + (1|mom),
-								 data=data.f.ars.ad, family = 'gaussian')
+DT.aad.18 <- DT[period == 'adult', {
+	m <- glmer(
+		log(annual_rs) ~
+			ego_period_rank +
+			scale(twi_degree) +
+			scale(aggr_degree) +
+			scale(affil_degree) +
+			offset(log(clan_size)) +
+			offset(log(nSession)) +
+			(1 | mom),
+		data = .SD,
+		family = 'gaussian'
+	)
+	tidy(m)
+}, by = iteration]
+
 
 # dAIC = 1.2
 a.ad.18 <- glmer(log(annual_rs) ~ ais_deg_scl
