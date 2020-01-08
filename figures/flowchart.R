2901dd98a8adafe2c9e30bff6ff47bc29a17b873
Alec Robitaille <robit.alec@gmail.com>
2020-01-08 15:30:43 -0330

    color by ego

--- figures/flowchart.R
+++ figures/flowchart.R
@@ -102,8 +102,8 @@ aggr[, idate := sessiondate]
 #  average of behavior1 during period
 avgLs <- foreach(i = seq(1, nrow(focal))) %do% {
 	f <- aggr[focal[i],
-								on = .(sessiondate >= period_start,
-											 sessiondate < period_end)]
+						on = .(sessiondate >= period_start,
+									 sessiondate < period_end)]
 	f[, .(avgB1 = mean(behavior1)), by = .(aggressor, recip)]
 }
 
@@ -111,9 +111,13 @@ avgLs <- foreach(i = seq(1, nrow(focal))) %do% {
 edgeLs <- foreach(i = seq(1, nrow(focal))) %do% {
 	twi <- data.table(melt(twiLs[[i]]), stringsAsFactors = FALSE)
 	twi[, c('Var1', 'Var2') := lapply(.SD, as.character), .SDcols = c(1, 2)]
-	merge(avgLs[[i]], twi,
-				by.x = c('aggressor', 'recip'),
-				by.y = c('Var1', 'Var2'), all.x = TRUE)
+	merge(
+		avgLs[[i]],
+		twi,
+		by.x = c('aggressor', 'recip'),
+		by.y = c('Var1', 'Var2'),
+		all.x = TRUE
+	)
 }
 
 # Generate graph and calculate network metrics
@@ -138,8 +142,8 @@ affil[, idate := sessiondate]
 # Count number of (directed) affiliations between individuals
 countLs <- foreach(i = seq(1, nrow(focal))) %do% {
 	f <- affil[focal[i],
-								 on = .(sessiondate >= period_start,
-								 			 sessiondate < period_end)]
+						 on = .(sessiondate >= period_start,
+						 			 sessiondate < period_end)]
 	f[, .N, .(ll_receiver, ll_solicitor)]
 }
 
@@ -147,8 +151,13 @@ countLs <- foreach(i = seq(1, nrow(focal))) %do% {
 edgeLs <- foreach(i = seq(1, nrow(focal))) %do% {
 	twi <- data.table(melt(twiLs[[i]]), stringsAsFactors = FALSE)
 	twi[, c('Var1', 'Var2') := lapply(.SD, as.character), .SDcols = c(1, 2)]
-	merge(countLs[[i]], twi, by.x = c('ll_receiver', 'll_solicitor'),
-				by.y = c('Var1', 'Var2'), all.x = TRUE)
+	merge(
+		countLs[[i]],
+		twi,
+		by.x = c('ll_receiver', 'll_solicitor'),
+		by.y = c('Var1', 'Var2'),
+		all.x = TRUE
+	)
 }
 
 # Generate graph and calculate network metrics
@@ -166,9 +175,9 @@ names(affilnets) <- paste0('affil-', focal$period)
 
 ### Plot nets
 nets <- rbindlist(lapply(c(assonets, aggrnets, affilnets),
-													 ggnetwork),
-										idcol = 'nm',
-										fill = TRUE)
+												 ggnetwork),
+									idcol = 'nm',
+									fill = TRUE)
 nets[, label := ifelse(vertex.names == selfocal, selfocal, ' ')]
 
 nets[, c('type', 'period') := tstrsplit(nm, '-')]
@@ -183,7 +192,7 @@ nets[type == 'aggr', type := 'Aggression']
 
 nets[, type := factor(type, levels = c('Association', 'Aggression', 'Affiliation'))]
 
-nets[, period := factor(period, levels=c('CD','DI','Adult'))]
+nets[, period := factor(period, levels = c('CD', 'DI', 'Adult'))]
 
 nets[, weightCut := cut(weight, breaks = 4), type]
 nets[type == 'Association', weightCut := cut(weight * 100, breaks = 4)]
@@ -191,13 +200,14 @@ nets[type == 'Association', weightCut := cut(weight * 100, breaks = 4)]
 # for aesthetic
 # nets[vertex.names == 'mono', c('x', 'y') := .(0.5, 0.5)]
 
-(gnets <- ggplot(nets,
-								 aes(
-								 	x = x,
-								 	y = y,
-								 	xend = xend,
-								 	yend = yend
-								 )) +
+(
+	gnets <- ggplot(nets,
+									aes(
+										x = x,
+										y = y,
+										xend = xend,
+										yend = yend
+									)) +
 		geom_edges(aes(alpha = weightCut),
 							 data = nets[type == 'Aggression']) +
 		geom_edges(aes(alpha = weightCut),
@@ -206,7 +216,7 @@ nets[type == 'Association', weightCut := cut(weight * 100, breaks = 4)]
 							 data = nets[type == 'Association']) +
 		geom_nodes() +
 		geom_nodes(
-			color = ifelse(selfocal == 'mono', 'blue', 'orange'),
+			color = ifelse(selfocal == 'mono', '#1b9e77', '#7570b3'),
 			shape = 19,
 			size = 4,
 			data = nets[vertex.names == selfocal]
@@ -222,7 +232,8 @@ focal[period == 'cd', period := 'CD']
 focal[period == 'postgrad', period := 'DI']
 focal[period == 'adult', period := 'Adult']
 
-(tmln <- ggplot(focal,
+(
+	tmln <- ggplot(focal,
 								 aes(x = period_start)) +
 		geom_hline(
 			yintercept = 0,
@@ -254,26 +265,21 @@ focal[period == 'adult', period := 'Adult']
 		)) +
 		guides(color = FALSE) +
 		scale_color_tableau(palette = "Color Blind") +
-		scale_y_continuous(expand = c(0,0.1)) +
-		scale_x_date(expand = c(0.1,0)) +
-		theme(aspect.ratio=0.13) +
+		scale_y_continuous(expand = c(0, 0.1)) +
+		scale_x_date(expand = c(0.1, 0)) +
+		theme(aspect.ratio = 0.13) +
 		geom_point(aes(period_start, 0)) +
 		geom_point(aes(period_end, 0))
 )
 
 
 ### Silhouette ----
-if (selfocal == 'mono') {
-	img <- rphylopic::image_data('f1b665ae-8fe9-42e4-b03a-4e9ae8213244', 512)[[1]]
-} else {
-	img <- rphylopic::image_data('a160fd31-c6ba-4bd1-b2a1-dbd77014139d', 512)[[1]]
-}
+img <- rphylopic::image_data('f1b665ae-8fe9-42e4-b03a-4e9ae8213244', 512)[[1]]
 
 (gimg <- ggplot() +
-	add_phylopic(img, 1) +
-	labs(caption = selfocaltitle)  +
-	theme(
-		plot.caption = element_text(hjust = 0.5, size = 15)))
+		add_phylopic(img, 1, color = ifelse(selfocal == 'mono', '#1b9e77', '#7570b3')) +
+		labs(caption = selfocaltitle)  +
+		theme(plot.caption = element_text(hjust = 0.5, size = 15)))
 
 
 ### Patchwork ----
@@ -306,8 +312,8 @@ ggsave(
 
 # Combine
 mono <- image_read('figures/flowchart-mono.png')
-arg <- image_read('figures/flowchart-arg.png')
-img <- c(mono, arg)
+gui <- image_read('figures/flowchart-gui.png')
+img <- c(mono, gui)
 image_write(image_append(img, stack = TRUE),
 						path = "figures/flowchart-i.png",
 						format = "png")
